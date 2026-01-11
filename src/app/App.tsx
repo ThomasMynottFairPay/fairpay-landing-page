6963891e42c3f6b9143ae484dd028c915fb3aa00
Thomas Mynott <thomasmynott@Mac.modem>
2026-01-11 21:26:18 +1100

    Implement routing for /churches

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/churches" element={<LandingPage />} />
+        <Route path="/" element={<Navigate to="/churches" replace />} />
+        {/* Catch all other routes and redirect to /churches */}
+        <Route path="*" element={<Navigate to="/churches" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
