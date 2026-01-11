e93bbf114b1d0362927ae64c297b0b7dae066417
Thomas Mynott <thomasmynott@Mac.modem>
2026-01-11 21:31:36 +1100

    Switch to subdomain strategy: Landing page at root

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/churches" element={<LandingPage />} />
-        <Route path="/" element={<Navigate to="/churches" replace />} />
-        {/* Catch all other routes and redirect to /churches */}
-        <Route path="*" element={<Navigate to="/churches" replace />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
